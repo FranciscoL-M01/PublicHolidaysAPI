599c0e6d69990b75621972be35782ed324511580
Francisco Lopez <francisco.lopezmorelos01@gmail.com>
2025-02-02 01:50:06 -0800

    Register our service to use our HolidaysApiService

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// AddSingleton method creates service for the first request only. Will be reused for every occuring request.
+builder.Services.AddSingleton<IHolidaysApiService, HolidaysApiService>();
 
 var app = builder.Build();
 
@@ -10,10 +12,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
