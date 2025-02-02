36801f74f58432e5804f64d1257839657b65bc72
Francisco Lopez <francisco.lopezmorelos01@gmail.com>
2025-02-02 01:53:10 -0800

    inject HolidaysApiService in the HomeController and call GetHolidays method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,24 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using PublicHolidaysAPI.Models;
+// inject HolidaysApiService and call the GetHolidaus method
 
-namespace PublicHolidaysAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IHolidaysApiService _holidaysApiService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IHolidaysApiService holidaysApiService)
     {
-        _logger = logger;
+        _holidaysApiService = holidaysApiService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(string countryCode, int year)
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        List<HolidayModel> holidays = new List<HolidayModel>();
+        // make sure that the website doesn't crash when a code or year isn't given
+        if (!string.IsNullOrEmpty(countryCode) && year > 0)
+        {
+            holidays = await _holidaysApiService.GetHolidays(countryCode, year);
+        }
+        return View(holidays);
     }
 }
